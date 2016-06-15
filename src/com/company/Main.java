0e57e5051868bbd6367fc1dc28c472ceb81a25e8
chupacabra <ryan.russell001@yahoo.com>
2016-06-15 00:53:16 -0700

    basic gui in a working state

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        omdbScraper omdbScraper = new omdbScraper();
-        omdbScraper.getStuff("Green Mile");
-       /* TrackerGui tracker = new TrackerGui();
+      //  omdbScraper omdbScraper = new omdbScraper();
+      //  omdbScraper.getStuff("Green Mile");
+        TrackerGui tracker = new TrackerGui();
         tracker.startTracker();
-       */
+
     }
 }
