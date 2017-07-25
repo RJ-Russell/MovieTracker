1cd92535b0c3df783e5bab07374489973853e3ef
RJ Russell <ryan.russell001@yahoo.com>
2017-07-25 02:39:52 -0600

    add function in main to display fonts. Need to find a different one for the GUI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -12,5 +13,12 @@ public class Main {
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
         TrackerGui tracker = new TrackerGui();
         tracker.startTracker();
+
+//        String fonts[]
+//                = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+//
+//        for (int i = 0; i < fonts.length; i++) {
+//            System.out.println(fonts[i]);
+//        }
     }
 }
